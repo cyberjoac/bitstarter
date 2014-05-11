585658d9d74a294bd2f43d15cfdb87dd286b19c5
Ubuntu <ubuntu@ip-172-31-39-29.us-west-2.compute.internal>
2014-05-11 14:46:16 +0000

    Buffer style send the response

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var express = require('express')
+var fs = require('fs')
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
+    var buffer = fs.readFileSync('index.html');
+    var indexContent = buffer.toString(); 
+    
+    response.send(indexContent);
 })
 
 app.listen(app.get('port'), function() {
